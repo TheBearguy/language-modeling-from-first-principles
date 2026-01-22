0de94dc42f3b855ab2599de741ec4789b0575b6b
TheBearguy <bugbreaker18@gmail.com>
2026-01-22 21:09:16 +0530

    train method : runs as orchestrator to build the vocabulary and the new corpus, iteratively, until the target size of vocab is achieved

--- bpe.py
+++ bpe.py
@@ -2,7 +2,7 @@
 # get_pair_counts()
 
 
-def train(text):
+def get_corpus(text):
     """
     takes in the text, 
     returns the corpus with which we play ahead
@@ -10,8 +10,9 @@ def train(text):
     words = text.split(" ")
     corpus = []
     for word in words: 
-        word += "</w>"
-        corpus.append(list(word))
+        char_list = list(word)
+        char_list.append('</w>')
+        corpus.append(char_list)
     return corpus
 
 
@@ -24,10 +25,10 @@ def get_pair_counts(corpus):
     counts = {}
     for word in corpus: 
         # print(f"\nWord in the method: {word}\n")
-        for i in range(len(word)-5): 
+        for i in range(len(word)-1): 
             pair = (word[i], word[i+1]) 
             counts[pair] = counts.get(pair, 0) + 1
-            print(f"Counts :: {counts}")
+            # print(f"Counts :: {counts}")
     return counts
 
 
@@ -42,8 +43,8 @@ def merge_pairs(corpus, pair_freq):
     for word in corpus: 
         new_word = []
         i = 0
-        while i < len(word) - 5: 
-            if i < len(word) - 5 and (word[i], word[i+1]) == pair_to_merge: 
+        while i < len(word) - 1: 
+            if i < len(word) - 1 and (word[i], word[i+1]) == pair_to_merge: 
                 merged_token = word[i] + word[i+1]
                 new_word.append(merged_token)
                 i += 2
@@ -51,15 +52,45 @@ def merge_pairs(corpus, pair_freq):
                 new_word.append(word[i])
                 i += 1
         new_corpus.append(new_word)
-    return new_corpus
+    return new_corpus, pair_to_merge 
+
+
+def train(target, text): 
+    corpus = get_corpus(text)
+    # Building the vocab - init stage
+    vocab = set()
+    for word in corpus: 
+        for char in word: 
+            vocab.add(char)
+
+    print(f"\nInitial Vocab size : {len(vocab)}")
+
+    while len(vocab) < target: 
+        pair_freq = get_pair_counts(corpus)
+        if not pair_freq: 
+            break
+        corpus, pair_to_merge = merge_pairs(corpus, pair_freq)
+
+        # Add merged token to vocab
+        merged_token = pair_to_merge[0] + pair_to_merge[1]
+        vocab.add(merged_token)
+
+        print(f"Merged {pair_to_merge} → vocab size: {len(vocab)}")
+    
+    print(f"\nFinal vocab :: {sorted(vocab)}")
+    print(f"\nFinal Corpus :: {corpus}")
+
+
 
 
 if __name__ == "__main__": 
-    target = 5
-    corpus = train("The penguin started heading towards the mountains; some 70 kms away")
-    # print(f"CORPUS :: {corpus}\n")
-    pair_freq = get_pair_counts(corpus)
-    print(f"\npairs = {pair_freq}\n")
-    # print(f"\nPAIRS Frequency :: {pair_freq}\n")
-    new_corpus = merge_pairs(corpus, pair_freq)
-    print(f"\nNEW CORPUS :: {new_corpus}\n")
+    target = 100
+    text = "The penguin started heading towards the mountains; some 70 kms away"
+    train(target, text)
+    # corpus = get_corpus("The penguin started heading towards the mountains; some 70 kms away")
+    # # print(f"CORPUS :: {corpus}\n")
+    # pair_freq = get_pair_counts(corpus)
+    # # print(f"\npairs = {pair_freq}\n")
+    # # print(f"\nPAIRS Frequency :: {pair_freq}\n")
+    # new_corpus = merge_pairs(corpus, pair_freq)
+    # print(f"\nNEW CORPUS :: {new_corpus}\n")
